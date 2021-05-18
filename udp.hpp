19c3fedcc79c6c84b5b45f9d123bdebfd05ddd59
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2021-05-18 13:47:14 +0200

    Minor windows compilation bug

--- udp.hpp
+++ udp.hpp
@@ -8,12 +8,12 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
-#include <Mswsock.h>
 #include <WinSock2.h>
 #include <iphlpapi.h>
 #include <stdio.h>
 #include <windows.h>
 #include <ws2tcpip.h>
+#include <Mswsock.h>
 #pragma comment(lib, "ws2_32.lib")
 #else
 #include <arpa/inet.h>
