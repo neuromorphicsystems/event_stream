b1a68cb47f9026b3be9754f2130da063460b2d51
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2024-05-31 19:34:17 +1000

    Build ARM wheels on Linux (this should include raspberry pi)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ elif sys.platform == "darwin":
 
 setuptools.setup(
     name="event_stream",
-    version="1.6.0",
+    version="1.6.1",
     url="https://github.com/neuromorphicsystems/event_stream",
     author="Alexandre Marcireau",
     author_email="alexandre.marcireau@gmail.com",
